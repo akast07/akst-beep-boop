c3fb1d5e9dd76a5ceceba5b8774ab61f064baecc
akast <dev.alejandrocastillo@gmail.com>
2020-01-16 17:20:42 -0600

    code changes

--- bot.js
+++ bot.js
@@ -15,10 +15,12 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
   
-  client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.channel.send('Pinging...').then((msg)=>{
+            msg.edit("Ping: " + (Date.now() - msg.createdTimestamp));
+        });
     }
-  });
-  
-  client.login(process.env.token);
+});
+
+client.login(process.env.token);
