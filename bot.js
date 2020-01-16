7aa1e644347e54077e8ae2fb09c4af554a37093c
akast <dev.alejandrocastillo@gmail.com>
2020-01-16 16:43:31 -0600

    added discord.js libary

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
-let Discord = require('discord.io');
+let dotenv = require('dotenv').config();
 let logger = require('winston');
 let auth = require('./auth.json');
-let dotenv = require('dotenv').config();
-
+const Discord = require('discord.js');
+//initialize bot
+const client = new Discord.Client();
 
 //logger settings 
 logger.remove(logger.transports.Console);
@@ -11,56 +12,14 @@ logger.add(new logger.transports.Console(),{
 });
 logger.level = 'debug';
 
-//initialize bot
-let bot = new Discord.Client({
-    "token":process.env.token,
-    "status":"dnd",
-    "afk":false
-});
-
-bot.on('on_error',(evt) => {
-    logger.info("-----Error time-----");
-    logger.info(bot);
-});
-
-process.on('uncaughtException', function(error) {
-    // do something clever here
-    logger.info("-----Error time-----");
-    logger.info(bot);
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
   });
-
-
-bot.on('ready', (evt) => {
-    logger.info('**Connected**');
-    logger.info('Logged in as: ');
-    logger.info(bot.username+'-('+bot.id+')');
-});
-
-bot.on('message', (user, userID, channelID, message, evt) => {
-
-    if (message.content != null) {
-        
-        if (message.member.voiceChannel) {
-            const channel = message.member.voiceChannel;
-            channel.join()
-            .then(connection => console.log('Connected!'))
-            .catch(console.error);
-            }
-
-        switch(message) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                case 'pong':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'ping!'
-                    });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+  
+  client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+  });
+  
+  client.login(process.env.token);
